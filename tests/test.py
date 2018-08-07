731faa798aeb1b6dea6e675087d96980fa08ff19
Xavier Barnada <xbarnada@gisce.net>
2018-08-07 09:08:31 +0200

    Fix syntax error

--- tests/test.py
+++ tests/test.py
@@ -169,7 +169,7 @@ class TestRecord(unittest.TestCase):
 
         rec = Record(1, rg)
         rg.records = [1]
-        rg.ensureRecordLoaded = empty_functionFalse)
+        rg.ensureRecordLoaded = empty_function
         rec.setValue("name", 1)
 
         rec.value("name")
