50e981537e6df1113bef4e4feac6c9a05a7da133
Xavier Barnada <xbarnada@gisce.net>
2019-03-21 15:38:33 +0100

    Bump to v6.0.7

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ Topic :: Office/Business
 
 setup(
     name='koo',
-    version='6.0.6',
+    version='6.0.7',
     packages=find_packages(),
     url='http://www.NaN-tic.com/koo-platform',
     license='GPL',
