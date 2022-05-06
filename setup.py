8fdbf088da5d038a4833d4c08c1e8417ae311c3d
Adrià Nicolau Beltran <anicolau@gisce.net>
2022-05-06 11:52:14 +0200

    Bump to v6.2.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ Topic :: Office/Business
 
 setup(
     name='koo',
-    version='6.2.2',
+    version='6.2.3',
     packages=find_packages(),
     url='http://www.NaN-tic.com/koo-platform',
     license='GPL',
