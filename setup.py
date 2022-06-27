a04457310fd87c2ed92ce7ddc5b26405552b8b37
Eduard Carrerars <ecarreras@gisce.net>
2022-06-27 16:40:29 +0200

    Bump to v6.3.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ Topic :: Office/Business
 
 setup(
     name='koo',
-    version='6.2.4',
+    version='6.3.0',
     packages=find_packages(),
     url='http://www.NaN-tic.com/koo-platform',
     license='GPL',
