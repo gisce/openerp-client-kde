211742d1afceac27e2d73bc18dc54077031f2092
Marcos Ameijeiras <118173541+mameijeiras@users.noreply.github.com>
2024-03-11 14:33:47 +0100

    Bump to v6.6.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ Topic :: Office/Business
 
 setup(
     name='koo',
-    version='6.5.0',
+    version='6.6.0',
     packages=find_packages(),
     url='http://www.NaN-tic.com/koo-platform',
     license='GPL',
